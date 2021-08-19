ab5f8ce0169deee09a9f41f867ce4d9d35163cb0
Ajit Singh <ajitsinghkaler0@gmail.com>
2021-08-19 13:25:01 +0530

    added basic comments section

--- src/app/pages/article-detail/article/comments/comments.component.ts
+++ src/app/pages/article-detail/article/comments/comments.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { CommentsStore } from '../services/comments.store';
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.scss']
+  styleUrls: ['./comments.component.scss'],
 })
 export class CommentsComponent implements OnInit {
+  comments$ = this.commentStore.comments$;
+  constructor(private commentStore: CommentsStore) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
